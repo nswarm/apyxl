8ecaa5fa7718dfbfe198b38c4322b05249173635
nswarm <nswarm.dev@gmail.com>
2023-04-02 16:44:42 -0700

    input::buffer: simplify

--- apyxl/src/input/buffer.rs
+++ apyxl/src/input/buffer.rs
@@ -4,35 +4,18 @@ use crate::input::Input;
 #[derive(Default)]
 pub struct Buffer {
     data: String,
-    read: bool,
 }
 
 impl Buffer {
     pub fn new(data: impl ToString) -> Self {
         Self {
             data: data.to_string(),
-            read: false,
         }
     }
 }
 
 impl Input for Buffer {
     fn next_chunk(&mut self) -> Option<&str> {
-        if self.read {
-            None
-        } else {
-            self.read = true;
-            Some(&self.data)
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    // todo test self.read
-
-    #[test]
-    fn asdf() {
-        todo!("nyi")
+        Some(&self.data)
     }
 }
