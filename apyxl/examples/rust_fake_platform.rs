65ff5f85c3dae9674cbe2bae164ca1ad7f161e44
nswarm <nswarm.dev@gmail.com>
2023-04-29 18:56:57 -0700

    fix fake platform path to not include src

--- apyxl/examples/rust_fake_platform.rs
+++ apyxl/examples/rust_fake_platform.rs
@@ -6,7 +6,7 @@ use std::path::PathBuf;
 fn main() -> Result<()> {
     env_logger::init();
     let examples_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?).join("examples");
-    let input_root = examples_dir.join("fake_platform");
+    let input_root = examples_dir.join("fake_platform/src");
     let output_root = examples_dir.join("output/rust_fake_platform");
     let mut input = input::Glob::new(&input_root, "**/*.rs")?;
     let mut output = output::FileSet::new(output_root)?;
