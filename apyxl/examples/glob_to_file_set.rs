6b103bf67934ec9c73f84abdecffb373e737f897
nswarm <nswarm.dev@gmail.com>
2023-04-30 20:44:06 -0700

    use file macro to get filename in examples

--- apyxl/examples/glob_to_file_set.rs
+++ apyxl/examples/glob_to_file_set.rs
@@ -7,7 +7,13 @@ fn main() -> Result<()> {
     env_logger::init();
     let examples_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?).join("examples");
     let input_root = examples_dir.join("simple_input");
-    let output_root = examples_dir.join("output/in_out_glob");
+    let file_name = PathBuf::from(file!())
+        .with_extension("")
+        .file_name()
+        .unwrap()
+        .to_string_lossy()
+        .to_string();
+    let output_root = examples_dir.join(format!("output/{}", file_name));
     let mut input = input::Glob::new(&input_root, "**/*.rs")?;
     let mut output = output::FileSet::new(output_root)?;
     Executor::default()
