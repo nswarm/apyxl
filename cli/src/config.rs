4cf9540c89b9561f3ff616fcf4d01a8ad0025d6e
nswarm <nswarm.dev@gmail.com>
2025-06-19 20:16:18 -0700

    remove example markdown generator

--- cli/src/config.rs
+++ cli/src/config.rs
@@ -73,7 +73,6 @@ pub enum ParserImpl {
 #[derive(ValueEnum, Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub enum GeneratorName {
     Rust,
-    Markdown,
 }
 
 #[derive(Clone, Debug)]
@@ -122,7 +121,6 @@ impl GeneratorName {
     pub fn create_impl(&self) -> Box<dyn apyxl::Generator> {
         match self {
             GeneratorName::Rust => Box::new(apyxl::generator::Rust::default()),
-            GeneratorName::Markdown => Box::new(apyxl::generator::Markdown::default()),
         }
     }
 }
