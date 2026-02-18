293a8ec347b033c41dd85dd9fc8fa7fee8633d9f
nswarm <nswarm.dev@gmail.com>
2026-02-18 07:56:21 -0800

    pyx: nested namespaces

--- apyxl/src/feature.rs
+++ apyxl/src/feature.rs
@@ -1,12 +1,14 @@
 #[derive(Debug, Copy, Clone, macros::ValidateFeatures)]
 pub enum Feature {
     Namespace,
+    NestedNamespace,
 }
 
 impl Feature {
     pub fn to_pyx_str(self) -> &'static str {
         match self {
             Feature::Namespace => "namespace ns {}",
+            Feature::NestedNamespace => "namespace outer { namespace inner {} }",
         }
     }
 }
--- apyxl/src/parser/pyx/namespace.rs
+++ apyxl/src/parser/pyx/namespace.rs
@@ -1,4 +1,4 @@
-use crate::model::Namespace;
+use crate::model::{Namespace, NamespaceChild};
 use crate::parser::pyx::token::Token;
 use crate::parser::pyx::Error;
 use crate::parser::Config;
@@ -14,49 +14,24 @@ where
 {
     let ident = select! { Token::Ident(ident) => ident };
 
-    just(Token::Namespace)
-        .ignore_then(ident)
-        .then_ignore(just(Token::Ctrl('{')))
-        .then_ignore(just(Token::Ctrl('}')))
-        .map_with(|name, e| {
-            Namespace {
-                name: Cow::Borrowed(name),
-                children: vec![],
-                attributes: Default::default(),
-                is_virtual: false,
-            }
-            .with_span(e.span())
-        })
-}
+    recursive(|nested| {
+        let children = nested
+            .map(|ns: Spanned<Namespace<'src>>| NamespaceChild::Namespace(ns.inner))
+            .repeated()
+            .collect::<Vec<_>>();
 
-// fn children<'src, I>(
-//     config: &'src Config,
-//     // namespace: impl Parser<'src, &'src str, Namespace<'src>, Error<'src>>,
-//     // end_delimiter: impl Parser<'src, &'src str, (), Error<'src>>,
-// ) -> impl Parser<'src, &'src str, Vec<NamespaceChild<'src>>, Error<'src>>
-// where
-//     I: ValueInput<'src, Token = Token<'src>, Span = SimpleSpan>,
-// {
-//
-//     // choice((
-//     //     crate::parser::rust::dto::parser(config).map(|(c, v)| Some((NamespaceChild::Dto(c), v))),
-//     //     crate::parser::rust::rpc::parser(config).map(|(c, v)| Some((NamespaceChild::Rpc(c), v))),
-//     //     crate::parser::rust::en::parser().map(|(c, v)| Some((NamespaceChild::Enum(c), v))),
-//     //     crate::parser::rust::ty_alias::parser(config).map(|(c, v)| Some((NamespaceChild::TypeAlias(c), v))),
-//     //     crate::parser::rust::namespace::field(config).map(|(c, v)| Some((NamespaceChild::Field(c), v))),
-//     //     namespace.map(|(c, v)| Some((NamespaceChild::Namespace(c), v))),
-//     //     crate::parser::rust::namespace::impl_block(config).map(|c| Some((NamespaceChild::Namespace(c), crate::parser::rust::visibility::Visibility::Public))),
-//     // ))
-//     //     .recover_with(skip_then_retry_until(
-//     //         any().ignored(),
-//     //         end_delimiter.ignored(),
-//     //     ))
-//     //     .map(|opt| match opt {
-//     //         Some((child, visibility)) => visibility.filter(child, config),
-//     //         None => None,
-//     //     })
-//     //     .repeated()
-//     //     .collect::<Vec<_>>()
-//     //     .map(|v| v.into_iter().flatten().collect_vec())
-//     //     .then_ignore(crate::parser::rust::comment::multi())
-// }
+        just(Token::Namespace)
+            .ignore_then(ident)
+            .then(children.delimited_by(just(Token::Ctrl('{')), just(Token::Ctrl('}'))))
+            .map_with(|(name, children), e| {
+                Namespace {
+                    name: Cow::Borrowed(name),
+                    children,
+                    attributes: Default::default(),
+                    is_virtual: false,
+                }
+                .with_span(e.span())
+            })
+            .boxed()
+    })
+}
